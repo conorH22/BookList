35c28ff2749ec4588a31afbde0638538d02cada7
conorH22 <lackamoredude@gmail.com>
2018-08-13 16:12:36 +0100

    application where you can add , store and delete the title, author and the ISBN number of a book. Local storage is used to store data.

---
+++ app.js
@@ -0,0 +1,104 @@
+//Book Constructor
+function Book (title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+//Add book to List
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    //create tr element
+    const row = document.createElement('tr');
+    //insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href = "#" class = "delete">x</a></td>
+    `;
+
+
+
+    list.appendChild(row);
+
+}
+//show alert
+UI.prototype.showAlert = function(message, className){
+    //create div
+    const div = document.createElement('div');
+    //add classes
+    div.className = `alert ${className}`;
+    //add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    //Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000 );
+}
+
+//delete book
+UI.prototype.deleteBook= function(target){
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
+
+//clear fields
+UI.prototype.clearFields= function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+
+//Event Listener for add book
+document.getElementById('book-form').addEventListener('submit', function(e){
+    //get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+          //Instantiate book
+          const book = new Book (title, author, isbn);
+
+          //Instantiate book
+          const ui = new UI();
+
+          //Validate
+          if(title === '' || author === '' ||isbn === ''){
+              //error alert
+              ui.showAlert('Please fill in all fields', 'error');
+          }else{
+
+         
+          //Add book to list
+
+          ui.addBookToList(book);
+          //show success
+          ui.showAlert('Book Added!', 'success');
+          //clear fields
+          ui.clearFields();
+
+          
+          }
+          e.preventDefault();
+
+});
+
+//Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+    //Instantiate book
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    //show message
+    ui.showAlert('Book Removed!','success')
+    e.preventDefault();
+})
